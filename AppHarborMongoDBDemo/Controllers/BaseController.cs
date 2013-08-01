2debb5fbc5f788a8687750fcce5efa768f86827b
GVK Someswara Rao <gvksrao@gmail.com>
2013-08-01 15:02:25 +0530

    New Build

---
+++ AppHarborMongoDBDemo/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+using MongoDB.Driver;
+
+namespace AppHarborMongoDBDemo.Controllers
+{
+	public abstract class BaseController : Controller
+	{
+		public MongoDatabase Database
+		{
+			get
+			{
+				return MongoDatabase.Create(GetMongoDbConnectionString());
+			}
+		}
+
+		private string GetMongoDbConnectionString()
+		{
+            string value = ConfigurationManager.AppSettings.Get("MONGOHQ_URL") ??
+                ConfigurationManager.AppSettings.Get("MONGOLAB_URI") ??
+                "mongodb://somesh:somesh@ds037768.mongolab.com:37768/someshdb";
+
+            return value;
+		}
+	}
+}
